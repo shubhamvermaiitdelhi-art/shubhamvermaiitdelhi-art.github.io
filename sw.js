2f37510c104c91b5da03c38c99396005e178a6b7
shubhamvermaiitdelhi-art <shubhamvermaiitdelhi@gmail.com>
2026-07-07 13:01:35 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* KhanaPro SW 2.0.23446 — self-updating */
-const V = "kp-2.0.23446";
+/* KhanaPro SW 2.0.23489 — self-updating */
+const V = "kp-2.0.23489";
 self.addEventListener("install", e => self.skipWaiting());
 self.addEventListener("activate", e => e.waitUntil(
   caches.keys().then(keys => Promise.all(keys.filter(k => k !== V).map(k => caches.delete(k)))).then(() => self.clients.claim())
